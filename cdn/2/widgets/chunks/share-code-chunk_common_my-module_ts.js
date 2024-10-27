9f2b5d6e9e46166ee56a569f4576446d869e13ce
owenngai <ngai91520@gmail.com>
2024-10-27 20:47:49 +0800

    udpate4

---
+++ cdn/2/widgets/chunks/share-code-chunk_common_my-module_ts.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkexb_client"] = self["webpackChunkexb_client"] || []).push([["your-extensions_widgets_share-code-chunk_common_my-module_ts"],{
+
+/***/ "./your-extensions/widgets/share-code-chunk/common/fn.ts":
+/*!***************************************************************!*\
+  !*** ./your-extensions/widgets/share-code-chunk/common/fn.ts ***!
+  \***************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   f: () => (/* binding */ f)
+/* harmony export */ });
+function f() {
+    return 'In f.';
+}
+
+
+/***/ }),
+
+/***/ "./your-extensions/widgets/share-code-chunk/common/my-module.ts":
+/*!**********************************************************************!*\
+  !*** ./your-extensions/widgets/share-code-chunk/common/my-module.ts ***!
+  \**********************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   f1: () => (/* binding */ f1)
+/* harmony export */ });
+/* harmony import */ var _fn__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./fn */ "./your-extensions/widgets/share-code-chunk/common/fn.ts");
+
+function f1() {
+    return `In f1. ${(0,_fn__WEBPACK_IMPORTED_MODULE_0__.f)()}`;
+}
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2lkZ2V0cy9jaHVua3Mvc2hhcmUtY29kZS1jaHVua19jb21tb25fbXktbW9kdWxlX3RzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBTyxTQUFTLENBQUM7SUFDZixPQUFPLE9BQU87QUFDaEIsQ0FBQzs7Ozs7Ozs7Ozs7Ozs7OztBQ0Z1QjtBQUVqQixTQUFTLEVBQUU7SUFDaEIsT0FBTyxVQUFVLHNDQUFDLEVBQUUsRUFBRTtBQUN4QixDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZXhiLWNsaWVudC8uL3lvdXItZXh0ZW5zaW9ucy93aWRnZXRzL3NoYXJlLWNvZGUtY2h1bmsvY29tbW9uL2ZuLnRzIiwid2VicGFjazovL2V4Yi1jbGllbnQvLi95b3VyLWV4dGVuc2lvbnMvd2lkZ2V0cy9zaGFyZS1jb2RlLWNodW5rL2NvbW1vbi9teS1tb2R1bGUudHMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGZ1bmN0aW9uIGYgKCkge1xuICByZXR1cm4gJ0luIGYuJ1xufSIsImltcG9ydCB7IGYgfSBmcm9tICcuL2ZuJ1xuXG5leHBvcnQgZnVuY3Rpb24gZjEgKCkge1xuICByZXR1cm4gYEluIGYxLiAke2YoKX1gXG59XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
