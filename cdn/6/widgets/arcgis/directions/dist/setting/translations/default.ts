db42a4b0f82aa7b803d9d916b365ecaa19613d9c
owenngai <ngai91520@gmail.com>
2024-10-27 19:01:13 +0800

    udpate2

--- cdn/6/widgets/arcgis/directions/dist/setting/translations/default.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  routeSettings: 'Route Settings',
-  searchSettings: 'Search settings',
-  routeUrl: 'Route URL',
-  outputStops: 'Stops',
-  outputRoute: 'Route',
-  outputDirectionPoints: 'Direction points',
-  outputDirectionLines: 'Direction lines'
-}
